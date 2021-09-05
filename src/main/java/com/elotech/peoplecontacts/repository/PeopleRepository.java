4e52f3a7e503217a414b237f7d01384edff1873e
DanRantino <danielaugustofurst@hotmail.com>
2021-09-05 00:20:01 -0300

    improvments

--- src/main/java/com/elotech/peoplecontacts/repository/PeopleRepository.java
+++ src/main/java/com/elotech/peoplecontacts/repository/PeopleRepository.java
@@ -2,6 +2,8 @@ package com.elotech.peoplecontacts.repository;
 
 import com.elotech.peoplecontacts.model.People;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeopleRepository extends JpaRepository<People, Integer> {
 }
